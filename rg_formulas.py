27877cc7d5a19b92d51a08628f2e458dad40c6e6
JayRizzo <Jahomie04@gmail.com>
2019-10-29 16:44:42 -0700

    Major overhaul to formulas
    updated to capture info by class's and reworking more, still a WIP.

--- rg_formulas.py
+++ rg_formulas.py
@@ -10,39 +10,126 @@ Specifically to showcase the some of the formulas in the game.
 # https://www.kongregate.com/forums/8945-realm-grinder/topics/509904-based-on-upgrades?page=10
 # =============================================================================
 from math import floor
+from pprint import pprint
 
 
-def research(reincarnation):
-    """What Research Points You Can have at your Reincarnation."""
-    research_points = (reincarnation) * (reincarnation + 1) // 2
-    print("R {} Research Points: {}".format(reincarnation, research_points))
-    return research_points
+class GeneralFormulas(object):
+    """Class for GeneralFormulas.
 
-research(10)
-research(20)
-research(30)
-research(40)
-research(50)
-research(60)
-research(70)
-research(80)
-research(82)
-research(90)
+    Attributes:
+        arg (TYPE): Description.
+    """
+
+    def __init__(self,
+                 reincarnation,
+                 lineage=0,
+                 spell_tiers=1,
+                 offtime=0,
+                 mana_regen=10,
+                 sp_duration=20,
+                 sp_cost=200
+                 ):
+        """Summary.
+
+        Args:
+            arg (TYPE): Description.
+        """
+        super(GeneralFormulas, self).__init__()
+        self.name = "General Formulas"
+        self.lineage_level = lineage + 0
+        self.reincarnation = reincarnation + 0
+        self.research_points = self.research(self.reincarnation)
+        self.offtime = offtime + 0
+        self.mana_regen = mana_regen + 0
+        self.sp_duration = sp_duration + 0
+        self.mana_regen = mana_regen + 0
+
+    def research(self, reincarnation):
+        """What Research Points You Can have at your Reincarnation.
+
+        Args:
+            reincarnation (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
+        """
+        self.reincarnation = reincarnation
+        research_points = (self.reincarnation) * (self.reincarnation + 1) // 2
+        print("R {} Research Points: {}".format(self.reincarnation,
+                                                research_points))
+        return research_points
+
+    def offline_bonus_spell(self, maxmana, reincarnation, tier):
+        """Offline bonus for spell tier.
+
+        Args:
+            maxmana (TYPE): Description.
+            reincarnation (TYPE): Description.
+            tier (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
+        """
+        offline_bonus = ((maxmana + 100 * self.reincarnation)**(1 +
+                                                                0.15 *
+                                                                tier))
+        return ("{:.2e}".format(offline_bonus))
+
+    def offline_sp_cast_time(self):
+        """Offline Activity Time Gain Formula.
+
+        http://musicfamily.org/realm/Spells/
+
+        Args:
+            offtime (TYPE): Description.
+            mana_regen (TYPE): Description.
+            sp_duration (TYPE): Description.
+            sp_cost (TYPE): Description.
+            spell_tiers (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
+        """
+        offline_spell_time = self.offtime * min(1, self.mana_regen *
+                                                self.sp_duration
+                                                (self.spell_tiers *
+                                                 self.sp_cost ** 1.5))
+        offline_spell_time2 = self.offtime * floor(self.mana_regen *
+                                                   self.sp_duration
+                                                   (self.spell_tiers *
+                                                    self.sp_cost ** 1.5))
+        return offline_spell_time, offline_spell_time2
 
 
 class Goblin(object):
-    """Goblin Faction."""
+    """Goblin Faction.
 
-    def __init__(self, arg):
+    Attributes:
+        factioncoin (int): Description.
+        lineage_level (int): Description.
+        name (str): Description.
+    """
+
+    def __init__(self, lineage):
         """Goblin Faction Init."""
         super(Goblin, self).__init__()
-        self.arg = arg
+        self.name = "Goblin"
+        self.lineage_level = lineage + 0
+        self.factioncoin = 0
 
     def goblins_greed(self, lineage, factioncoin):
-        """Spell Effect: Increase the production of all buildings."""
+        """Spell Effect: Increase the production of all buildings.
+
+        Args:
+            lineage (TYPE): Description.
+            factioncoin (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
+        """
         formula = ('FC: floor(ln3(1 + x)) + 20, x is gems '
                    'Effect: 0.75 * ln3.15(1 + x)%, x is faction coins found')
-        answer = floor((lineage ** 3 + (1 + factioncoin) + 20))
+        answer = floor((self.lineage_level ** 3) * (1 + self.factioncoin) + 20)
         print("Goblin's Greed Formula: {}".format(formula))
         print("Goblin's Greed  : {0:.3E}".format(answer))
         return answer
@@ -51,9 +138,16 @@ class Goblin(object):
         """Effect: Increase the production of all buildings.
 
         Based on the amount of Goblin Coins found in this game.
+
+        Args:
+            factioncoin (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
         """
-        formula = (1 + factioncoin) ** 0.03
+        formula = '(1 + factioncoin) ** 0.03'
         answer = (1 + factioncoin) ** 0.03
+        print("Increase production of all buildings: {}".format(answer))
         print("Strong Currency Formula: {}".format(formula))
         print("Strong Currency: {0:.3E}".format(answer))
         return answer
@@ -63,35 +157,87 @@ class Goblin(object):
 
                    based on upgrades purchased.
 
-        Formula: x ^ 1.2%, where x is your Upgrades Purchased (This Game) stat.
+        Formula: x **1.2%, where x is your Upgrades Purchased (This Game) stat.
+
+        Args:
+            upgradespurchased (TYPE): Description.
+
+        Returns:
+            TYPE: Description.
         """
-        formula = ('Formula: x ^ 1.2%, where x is your Upgrades Purchased '
+        formula = ('Formula: x ** 1.2%, where x is your Upgrades Purchased '
                    '(This Game) stat.')
         answer = (upgradespurchased) ** 1.2
         print("Goblin Economists Formula: {}".format(formula))
         print("Goblin Economists: {0:.3E}".format(answer))
         return answer
 
+    def printaname(self):
+        """Goblin Faction Init.
 
-Goblin.goblins_greed(None, 31, 4.62e29)
-Goblin.strong_currency(None, 4.62e29)
-Goblin.goblins_economists(None, 15)
+        Returns:
+            TYPE: Description.
+        """
+        print("Faction: {}".format(self.name))
+        return self.name
+
+    def main(self):
+        """Goblin Faction Init."""
+        self.printaname()
+        self.strong_currency(1e9)
 
 
-# python3 ~/GitHubz/rg_code/rg_formulas.py
-# R 10 Research Points: 55
-# R 20 Research Points: 210
-# R 30 Research Points: 465
-# R 40 Research Points: 820
-# R 50 Research Points: 1275
-# R 60 Research Points: 1830
-# R 70 Research Points: 2485
-# R 80 Research Points: 3240
-# R 82 Research Points: 3403
-# R 90 Research Points: 4095
-# Goblin's Greed Formula: FC: floor(ln3(1 + x)) + 20, x is gems Effect: 0.75 * ln3.15(1 + x)%, x is faction coins found
-# Goblin's Greed  : 4.620E+29
-# Strong Currency Formula: 7.761385577498356
-# Strong Currency: 7.761E+00
-# Goblin Economists Formula: Formula: x ^ 1.2%, where x is your Upgrades Purchased (This Game) stat.
-# Goblin Economists: 2.578E+01
+def dump(obj):
+    """Dumper.
+
+    Args:
+        obj (TYPE): Description.
+    """
+    for attr in dir(obj):
+        print("obj.%s = %r" % (attr, getattr(obj, attr)))
+
+
+if __name__ == "__main__":
+    a = GeneralFormulas(10)
+    a.research(10)
+    a.research(20)
+    a.research(30)
+    a.research(40)
+    a.research(50)
+    a.research(60)
+    a.research(70)
+    a.research(80)
+    a.research(82)
+    a.research(90)
+    a.offline_bonus_spell(182200, 63, 1)
+    a.offline_bonus_spell(182200, 63, 2)
+    a.offline_bonus_spell(182200, 63, 3)
+    a.offline_bonus_spell(182200, 63, 4)
+    a.offline_bonus_spell(182200, 63, 5)
+    a.offline_bonus_spell(182200, 63, 6)
+    a.offline_sp_cast_time(60, 100000, 20, 320, 5)
+    a.offline_sp_cast_time(600, 118341, 24, 500, 5)
+    b = Goblin()
+    b.goblins_greed(lineage=30, factioncoin=2994004)
+    b.goblins_greed(31, 4.62e29)
+    b.strong_currency(factioncoin=2994004)
+    b.strong_currency(4.62e29)
+    b.goblins_economists(15)
+    print("{}".format(a.__dict__))
+    print("{}".format(a.__dict__.keys()))
+    print("{}".format(a.__dict__.values()))
+    print("{}".format(a.__dir__()))
+    print("{}".format(dir(Goblin)))
+    print("{}".format(vars(Goblin)))
+    print("{}".format(', '.join(i for i in dir(Goblin)
+                                if not i.startswith('__'))))
+    for att in dir(a):
+        print("{} - {}".format(att, getattr(a, att)))
+    for att in dir(b):
+        print("{} - {}".format(att, getattr(b, att)))
+    pprint("{}".format(vars(a)))
+    pprint("{}".format(a))
+    pprint("{}".format(vars(a)))
+    pprint("{}".format(vars(a.element)))
+    dump("{}".format(a))
+    dump("{}".format(b))

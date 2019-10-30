3e27679a4ed6c20d7a07f3930d399f5b79b45f17
JayRizzo <Jahomie04@gmail.com>
2019-10-29 20:20:05 -0700

    Update xy_cord layout
    updated and added human_variance to start to simulate a more human like click speed.

--- class_act/buybuildings.py
+++ class_act/buybuildings.py
@@ -7,25 +7,74 @@ Buy all available Buildings.
 """
 # =============================================================================
 from random import choice
+from random import uniform
 
 from detectwindowsize import DetectWindowSize
 
 from pyautogui import click
+from pyautogui import easeInBounce
+from pyautogui import easeInElastic
+from pyautogui import easeInOutQuad
+from pyautogui import easeInQuad
+from pyautogui import easeOutQuad
+# from pyautogui import moveTo
 
 detectws = DetectWindowSize()
 DetectWindowSize.main(detectws)
-
 ADJUSTMENTS = detectws.adjustments
 X_CORDINATE = [900, ]
 Y_CORDINATE = [140, 200, 260, 320, 380, 440, 500, 560, 620, 680, 740, ]
 CLICK_SPEED = 0.001  # RG Lags a bit but works this fast for buildings
+RANDOM_MOUSE = [easeInQuad,
+                easeOutQuad,
+                easeInOutQuad,
+                easeInBounce,
+                easeInElastic
+                ]
+
+
+# # @CallCount
+# def buy_allbauildings(buy_clicks=1):
+#     """Docs."""
+#     # print("%s Buying all available buildings." % (time.ctime()))
+#     Y_CORDINATE[0],
+#     Y_CORDINATE[1],
+#     Y_CORDINATE[2],
+#     Y_CORDINATE[3],
+#     Y_CORDINATE[4],
+#     Y_CORDINATE[5],
+#     Y_CORDINATE[6],
+#     Y_CORDINATE[7],
+#     Y_CORDINATE[8],
+#     Y_CORDINATE[9],
+#     Y_CORDINATE[10],
+#     return
+
+
+def human_variance():
+    """The Module change the speed for each round of clicks you do.
+
+    Returns: Float(Number) between 1.00 <=> 0.00
+    """
+    z = float("{:00.2f}".format(round(uniform(0, 100) * .01, 2)))
+    return z
 
 
 def buyrandombuildings(clicks):
     """The Module buy all buildings at random."""
     clicks += 0
-    selectedz = choice(Y_CORDINATE)
-    click(X_CORDINATE[0], selectedz + ADJUSTMENTS, clicks, CLICK_SPEED)
+    selectedz = choice(Y_CORDINATE) + ADJUSTMENTS
+    # moveTo(x=X_CORDINATE[0],
+    #        y=selectedz,
+    #        duration=round(uniform(25, 100) * .01, 2),
+    #        tween=choice(RANDOM_MOUSE)
+    #        )
+    click(x=X_CORDINATE[0],
+          y=selectedz,
+          clicks=clicks,
+          interval=CLICK_SPEED
+          # interval=human_variance()
+          )
     return clicks
 
 
@@ -55,3 +104,11 @@ if __name__ == "__main__":
     buyrandombuildings(0)
     buybuildings(0)
     buybuildingsrev(0)
+
+
+# help(click)
+# click(x=None, y=None, clicks=1, interval=0.0, button='left', duration=0.0,
+#       tween=<function linear at 0x10fff20e0>, pause=None, _pause=True)
+# help(moveTo)
+# moveTo(x=None, y=None, duration=0.0, tween=<function linear at 0x10fff20e0>,
+#        pause=None, _pause=True)
--- class_act/clickmove.py
+++ class_act/clickmove.py
@@ -7,59 +7,51 @@ And is defining either `strightclickin` for Neutral or Evil Alignment.
 And is defining either `clickmove` for Good Alignment,
 To get the most from Dwarven upgrades.
 """
-import random
+
+from random import randint
+from random import uniform
 
 from detectwindowsize import DetectWindowSize
 
 from pyautogui import click
 from pyautogui import easeInOutQuad
+from pyautogui import moveTo
+
+dws = DetectWindowSize()
+DetectWindowSize.main(dws)
+X_CORD = dws.x_cordinate
+Y_CORD = dws.y_cordinate
+ADJUST = dws.adjustments
 
 
 def strightclickin(count_me, click_speed=0.037):
     """Docs."""
     count_me += 0
-    dws = DetectWindowSize()
-    DetectWindowSize.main(dws)
-    x_cordinate = dws.x_cordinate
-    y_cordinate = dws.y_cordinate
-    click(x_cordinate,
-          y_cordinate,
-          clicks=count_me,
-          interval=click_speed,
-          button='left'
-          )
+    moveTo(X_CORD, Y_CORD, round(uniform(1, 100) * .01, 2))
+    click(X_CORD, Y_CORD, clicks=count_me, interval=click_speed, button='left')
     return count_me
 
 
 def clickmove(count_me=50, click_speed=0.037):
-    """"The clickmove is a Module is for Good Players using Dwarven.
+    """"The clickmove is a Module is for Good Factions using Dwarven upgrade.
 
     This func is required to keep making the most money while clicking.
     you must move every 25-50 clicks otherwise you loose the %600 bonus.
-
     Required only for R2-R50?  Goes away after some point.
     """
-    dws = DetectWindowSize()
-    DetectWindowSize.main(dws)
-    x_cordinate = dws.x_cordinate
-    y_cordinate = dws.y_cordinate
-    adjustments = dws.adjustments
-
     count = 1
     count_upto_me = count_me
     _start_number = 0
     _interval = 25
     total_clicks_left = int(count_upto_me)
-
     for count in range(_start_number, count_upto_me, _interval):
-        click(x_cordinate + (random.randint(-25, 25)) + adjustments,
-              y_cordinate + (random.randint(-25, 25)) + adjustments,
+        click(X_CORD + (randint(-25, 25)) + ADJUST,
+              Y_CORD + (randint(-25, 25)) + ADJUST,
               clicks=_interval,
               interval=click_speed,
               button='left',
               tween=easeInOutQuad
               )
-
         count += _interval
         total_clicks_left -= _interval
     return count_me
